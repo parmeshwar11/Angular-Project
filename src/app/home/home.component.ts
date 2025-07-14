0cf79d06304a1b1f30dd9b6905f64fa8af85a0b2
parmeshwar11 <94509730+parmeshwar11@users.noreply.github.com>
2025-07-14 23:17:30 +0530

    added theme implementation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as mammoth from 'mammoth';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-})
-export class HomeComponent implements OnInit {
+    templateUrl: './home.component.html',
+      styleUrls: ['./home.component.css']
+      })
+      export class HomeComponent {
+        docxHtml: string = '';
+          isDarkMode = false;
 
-  constructor() { }
+            onFileChange(event: any): void {
+                const file = event.target.files[0];
+                    if (file && file.name.endsWith('.docx')) {
+                          const reader = new FileReader();
+                                reader.onload = async (e: any) => {
+                                        const arrayBuffer = e.target.result;
+                                                const result = await mammoth.convertToHtml({ arrayBuffer });
+                                                        this.docxHtml = result.value;
+                                                              };
+                                                                    reader.readAsArrayBuffer(file);
+                                                                        } else {
+                                                                              alert('Please upload a valid .docx file.');
+                                                                                  }
+                                                                                    }
 
-  ngOnInit(): void {
-  }
-
-}
+                                                                                      toggleTheme(): void {
+                                                                                          this.isDarkMode = !this.isDarkMode;
+                                                                                            }
+                                                                                            }
+                                                                                            
